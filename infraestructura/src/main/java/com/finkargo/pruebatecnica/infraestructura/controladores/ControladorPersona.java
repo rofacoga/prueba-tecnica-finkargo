5ed34e0640cbeaad22ac31767feebe3a0d64b874
Roger Córdoba García <rofacoga@gmail.com>
2022-10-17 17:31:01 -0500

    HU01: (Buscar personas): Se realiza ajuste del controlador y agrega primera parte de pruebas de integración del controlador de personas.

--- infraestructura/src/main/java/com/finkargo/pruebatecnica/infraestructura/controladores/ControladorPersona.java
+++ infraestructura/src/main/java/com/finkargo/pruebatecnica/infraestructura/controladores/ControladorPersona.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping(path = "/personas")
 public class ControladorPersona {
 
